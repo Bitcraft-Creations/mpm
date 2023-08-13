e310fe97847bff54b4a7cb0612a8095b80a3ef28
Joris Schelfhout <joris@shelfwood.co>
2023-08-13 22:03:04 +0200

    Add startup command

--- core.lua
+++ core.lua
@@ -40,6 +40,28 @@ function Core.list()
     end
 end
 
+-- Function to set a package as the startup script
+function Core.startup()
+    -- Prompt user for package name
+    print("Enter the name of the package you wish to set as the startup script:")
+    local package = read()
+
+    -- Prompt user for optional parameters
+    print("Enter any optional parameters to pass to the package (leave blank for none):")
+    local parameters = read()
+
+    -- Construct the startup script content
+    local startup_content = 'shell.run("mpm update")\\n'
+    startup_content = startup_content .. 'shell.run("mpm/packages/' .. package .. '.lua ' .. parameters .. '")\\n'
+
+    -- Write the startup script content to startup.lua
+    local file = fs.open("/startup.lua", "w")
+    file.write(startup_content)
+    file.close()
+
+    print("Startup script set successfully!")
+end
+
 -- Function to install a package
 function Core.install(package)
     -- Iterate over all repositories
