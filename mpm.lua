3143dc906d53f99600e6c7771f63d213626c6c99
djquinten <quintenvanderlaan@quintens-air.kpn>
2024-05-26 16:51:56 +0200

    Added mpm function

--- mpm.lua
+++ mpm.lua
@@ -1,4 +1,15 @@
 -- mpm.lua
+
+function mpm(module)
+    local path = "/mpm/packages/" .. module .. ".lua"
+
+    if fs.exists(path) then
+        return dofile(path)
+    end
+
+    error("Module '" .. module .. "' not found in /mpm/packages")
+end
+
 local bootstrap = dofile("/mpm/bootstrap.lua")
 
 -- Get the command-line arguments
