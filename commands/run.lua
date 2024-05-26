da376e47fb75ac5155faa7798a7f8d6681e1b764
djquinten <quintenvanderlaan@quintens-air.kpn>
2024-05-26 17:11:38 +0200

    Removed global mpm function and moved function to run.lua

--- commands/run.lua
+++ commands/run.lua
@@ -1,5 +1,15 @@
 local runModule = nil
 
+function mpm(module)
+    local path = "/mpm/packages/" .. module .. ".lua"
+
+    if fs.exists(path) then
+        return dofile(path)
+    end
+
+    error("Module '" .. module .. "' not found in /mpm/packages")
+end
+
 runModule = {
     usage = "mpm remove <package>",
 
--- mpm.lua
+++ mpm.lua
@@ -1,17 +1,4 @@
 -- mpm.lua
-
-function mpm(module)
-    local path = "/mpm/packages/" .. module .. ".lua"
-
-    if fs.exists(path) then
-        return dofile(path)
-    end
-
-    error("Module '" .. module .. "' not found in /mpm/packages")
-end
-
-_G.mpm = mpm
-
 local bootstrap = dofile("/mpm/bootstrap.lua")
 
 -- Get the command-line arguments
