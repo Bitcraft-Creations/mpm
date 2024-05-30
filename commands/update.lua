5d5c46c5cee9a7483ba26d4c93368da6e528eeef
Joris Schelfhout <joris@shelfwood.co>
2024-05-30 20:46:19 +0200

    Refactor updateModule.lua for improved readability and maintainability

--- commands/update.lua
+++ commands/update.lua
@@ -7,54 +7,41 @@ updateModule = {
 
     run = function(...)
         local names = {...}
-        local updatedComponents = {}
 
         print("Checking for updated modules...")
 
-        if #names == 0 then
-            local module_dirs = fs.list("/mpm/packages/")
-            for _, module_dir in ipairs(module_dirs) do
-                print("  - @" .. module_dir)
-                updateModule.updatePackagesInModule("/mpm/packages/" .. module_dir, updatedComponents)
-            end
-        else
+        -- Update all the specified packags e.g. mpm update a b c
+        if #names > 0 then
             for _, name in ipairs(names) do
-                updateModule.updateSingleComponent(name, updatedComponents)
+                updateModule.updateSingleComponent(name)
             end
-        end
-
-        if #updatedComponents <= 0 then
-            print("\nNo updates found.")
             return
         end
 
-        print("\nUpdated components:")
-        for _, component in ipairs(updatedComponents) do
-            print(component)
+        -- Update all modules if no package is specified
+        local module_dirs = fs.list("/mpm/packages/")
+
+        for _, module_dir in ipairs(module_dirs) do
+            print("  - @" .. module_dir)
+            updateModule.updatePackagesInModule("/mpm/packages/" .. module_dir)
         end
     end,
 
-    updatePackagesInModule = function(moduleDir, updatedComponents)
+    updatePackagesInModule = function(moduleDir)
         local packageFiles = fs.list(moduleDir)
-        local updated = false
         local moduleName = fs.getName(moduleDir)
         for _, packageFile in ipairs(packageFiles) do
-            if not packageFile:match("%.lua$") then -- Check if it's a Lua file
+            if not packageFile:match("%.lua$") then
                 goto continue
             end
 
             local package_name = fs.combine(moduleName, packageFile:match("(.+)%..+$")) -- Construct the package name
-            if updateModule.updateSingleComponent(package_name, updatedComponents, "  - @" .. moduleName) then
-                updated = true
-            end
+            updateModule.updateSingleComponent(package_name, "  - @" .. moduleName)
             ::continue::
         end
-        return updated
     end,
 
-    updateSingleComponent = function(name, updatedComponents, prefix)
-        local updated = false
-        prefix = prefix or ""
+    updateSingleComponent = function(name)
         -- Check if it's a package or a module
         if string.find(name, "/") then
             -- It's a package
@@ -62,7 +49,7 @@ updateModule = {
             local response = http.get(package_url)
             if not response then
                 print("Failed to fetch package: " .. name)
-                return updated
+                return
             end
             local newContent = response.readAll()
             response.close()
@@ -82,26 +69,22 @@ updateModule = {
                 local file = fs.open("/mpm/packages/" .. name .. ".lua", "w")
                 file.write(newContent)
                 file.close()
-                updated = true
-                table.insert(updatedComponents, prefix .. "  - " .. fs.getName(name) .. " (updated)")
+                print("  - " .. fs.getName(name) .. " (updated)")
             end
-        else
-            -- It's a module, update its file list first
-            if not installModule.downloadFile(repositoryUrl .. "/" .. name .. "/filelist.lua",
-                "/mpm/packages/" .. name .. "/filelist.lua") then
-                print("Failed to update file list for: " .. name)
-                return updated
-            end
-
-            local module_filelist = dofile("/mpm/packages/" .. name .. "/filelist.lua")
-            for _, package_name in ipairs(module_filelist) do
-                if updateModule.updateSingleComponent(fs.combine(name, package_name), updatedComponents,
-                    prefix .. "  - @" .. name) then
-                    updated = true
-                end
-            end
+            return
         end
-        return updated
+
+        -- It's a module, update its file list first
+        if not installModule.downloadFile(repositoryUrl .. "/" .. name .. "/filelist.lua",
+            "/mpm/packages/" .. name .. "/filelist.lua") then
+            print("Failed to update file list for: " .. name)
+            return
+        end
+
+        local module_filelist = dofile("/mpm/packages/" .. name .. "/filelist.lua")
+        for _, package_name in ipairs(module_filelist) do
+            updateModule.updateSingleComponent(fs.combine(name, package_name), prefix .. "  - @" .. name)
+        end
     end
 }
 
