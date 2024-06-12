75b764f5d23dc88d7eacb6b6afa25b717f545152
Joris Schelfhout <joris@shelfwood.co>
2024-06-12 20:54:26 +0200

    Refactor file paths in Update.lua for better organization and consistency

--- Core/Commands/Update.lua
+++ Core/Commands/Update.lua
@@ -1,4 +1,4 @@
-local updateModule = nil
+local this = nil
 local repositoryUrl = "https://shelfwood-mpm-packages.netlify.app/"
 local installModule = dofile("/mpm/Core/Commands/install.lua")
 
@@ -10,49 +10,49 @@ local installModule = dofile("/mpm/Core/Commands/install.lua")
     - Replace the existing module with the new module
     - For any modules that are no longer in the manifest, delete them
 ]]
-updateModule = {
+this = {
     usage = "mpm update <module>",
 
     run = function(...)
         local names = {...}
         -- If <module> names are specified, we only update those modules
         if #names > 0 then
-            updateModule.updateModules(names)
+            this.updateModules(names)
         end
 
         -- If no <module> names are specified, we update all modules
-        updateModule.updateModules(exports("Utils.File").list("/mpm/Packages/"))
+        this.updateModules(exports("Utils.File").list("/mpm/Packages/"))
     end,
 
     updateModules = function(modules)
         for _, module in ipairs(modules) do
-            updateModule.updateSingleModule(module)
+            this.updateModule(module)
         end
     end,
 
-    updateSingleModule = function(module)
+    updateModule = function(module)
         print("- @" .. module)
         local manifest = textutils.unserialiseJSON(http.get(repositoryUrl .. module .. "/manifest.json").readAll())
         for _, moduleName in ipairs(manifest.modules) do
-            updateModule.updateSingleFile(module, moduleName)
+            this.updateFile(module, moduleName)
         end
 
-        updateModule.removeFilesNotInList(module, manifest.modules)
+        -- this.removeFilesNotInList(module, manifest.modules)
     end,
 
-    updateSingleFile = function(module, filename)
+    updateFile = function(module, filename)
         -- Obtain the file from the repository
-        local content = http.get(repositoryUrl .. module .. "/" .. filename .. ".lua")
+        local content = http.get(repositoryUrl .. module .. "/" .. filename).readAll()
 
         -- If the file content is the same, return
-        local installedContent = exports("Utils.File").get("/mpm/Packages/" .. module .. "/" .. filename .. '.lua')
+        local installedContent = exports("Utils.File").get("/mpm/Packages/" .. module .. "/" .. filename)
 
         if installedContent == content then
             return
         end
 
         -- Replace the existing file with the new updated file
-        exports("Utils.File").put("/mpm/Packages/" .. module .. "/" .. filename .. '.lua', content)
+        exports("Utils.File").put("/mpm/Packages/" .. module .. "/" .. filename, content)
 
         -- Print the file name
         print("  - " .. filename)
@@ -61,7 +61,7 @@ updateModule = {
     removeFilesNotInList = function(module, modules)
         local files = exports("Utils.File").list("/mpm/Packages/" .. module)
         for _, file in ipairs(files) do
-            if not updateModule.isInList(file, modules) then
+            if not this.isInList(file, modules) then
                 exports("Utils.File").delete("/mpm/Packages/" .. module .. "/" .. file)
                 -- Print the file name with an X to indicate it is deleted
                 print("X - " .. file)
@@ -79,4 +79,4 @@ updateModule = {
     end
 }
 
-return updateModule
+return this
