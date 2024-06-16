b6dcdcdfb9d9903c17a831164c339b301af60220
Joris Schelfhout <joris@shelfwood.co>
2024-06-16 22:32:49 +0200

    Refactor file paths in Remove.lua for better organization and consistency

--- Core/Commands/Remove.lua
+++ Core/Commands/Remove.lua
@@ -1,10 +1,26 @@
 local removeModule = nil
 
 removeModule = {
-    usage = "mpm remove <package>",
+    usage = "mpm remove <package> <optional:package_2> etc.",
 
-    run = function(package)
-        exports("Utils.PackageDisk").remove(package)
+    run = function(...)
+        local packages = {...}
+
+        if #packages == 0 then
+            print("Please specify one or more packages to remove.")
+            return
+        end
+
+        for _, package in ipairs(packages) do
+            if not exports("Utils.PackageDisk").isInstalled(package) then
+                print("Package " .. package .. " is not installed.")
+                goto nextPackage
+            end
+
+            exports("Utils.PackageDisk").remove(package)
+
+            ::nextPackage::
+        end
     end
 }
 
